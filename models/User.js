9ef9271577229d131fb0e06415e2e259a6f22616
Prashant-AK <ps.prashantsingh.98@gmail.com>
2022-07-19 18:05:45 +0530

    change in mongoose file

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const { Schema, model } = require('mongoose');
 const UserSchema = new Schema(
   {
     name: { type: String, require: true },
-    email: { type: String, require: true },
-    username: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    username: { type: String, require: true, unique: true },
     password: { type: String, require: true },
     role: {
       type: String,
